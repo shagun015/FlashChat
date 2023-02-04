0b32f0fe56b00ea2425f8e80690cdd33136e962a
shagun yadav <shagunyadav8563@gmail.com>
2023-02-05 00:30:07 +0530

    final commit

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Shagun on 12/10/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
